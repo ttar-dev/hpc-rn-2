a2304dad5432901865baa5859241502c26a2cf12
Nattawut Kitiwan <ta.nattawut@icloud.com>
2024-01-03 22:09:05 +0700

    Refactor App component structure

--- App.tsx
+++ App.tsx
@@ -1,30 +1,16 @@
 import {StatusBar} from 'expo-status-bar';
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
-import Question from './components/Question';
-import {QUESTIONS, QuestionType} from './constants';
+
+import Navigation from './Navigation';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaView style={[styles.container, {marginHorizontal: 16}]}>
-      <StatusBar style="auto" />
-      <ScrollView style={{gap: 20, flex: 1}}>
-        <View style={{paddingVertical: 16}}>
-          <Text style={{fontSize: 40, fontWeight: 'bold'}}>Questions</Text>
-        </View>
-        <View style={{gap: 8}}>
-          {QUESTIONS.map((q: QuestionType, i: number) => (
-            <Question key={i} q={q} choiceNo={i + 1} />
-          ))}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <StatusBar style="auto" />
+        <Navigation />
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 4,
-  },
-});
