9adc1fcce9a610863fd2ace1f4ea7449fa49878a
Nattawut Kitiwan <ta.nattawut@icloud.com>
2024-01-03 22:49:55 +0700

    Add SQLite database initialization in App component

--- App.tsx
+++ App.tsx
@@ -5,8 +5,19 @@ import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {ThemeProvider} from '@rneui/themed';
 import theme from './themeConfig';
+import {useEffect} from 'react';
+import {db} from './utils/sqlite';
 
 export default function App() {
+  useEffect(() => {
+    db.transaction(tx => {
+      // tx.executeSql("DROP TABLE scores;");
+
+      tx.executeSql(
+        'CREATE TABLE IF NOT EXISTS scores (id integer primary key not null, nickname text, score int);',
+      );
+    });
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
