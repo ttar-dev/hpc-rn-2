399113e6d1bfe55d9fafe2f4255d93aa825a55aa
Nattawut Kitiwan <ta.nattawut@icloud.com>
2024-01-03 21:54:26 +0700

    Add constants file with question data

---
+++ constants.ts
@@ -0,0 +1,108 @@
+export interface QuestionType {
+  question: string;
+  choices: string[];
+  correctAnswer: string;
+}
+
+export const QUESTIONS = [
+  {
+    question: 'What color is the sky?',
+    choices: ['Red', 'Blue', 'Yellow', 'Green'],
+    correctAnswer: 'Blue',
+  },
+  {
+    question: 'Which color is a banana?',
+    choices: ['Purple', 'Orange', 'Green', 'Blue'],
+    correctAnswer: 'Yellow',
+  },
+  {
+    question: 'What color is the sun?',
+    choices: ['Blue', 'Yellow', 'Pink', 'Brown'],
+    correctAnswer: 'Yellow',
+  },
+  {
+    question: 'Which color is grass?',
+    choices: ['Red', 'Green', 'Yellow', 'Orange'],
+    correctAnswer: 'Green',
+  },
+  {
+    question: 'What color is an apple?',
+    choices: ['Blue', 'Green', 'Purple', 'Red'],
+    correctAnswer: 'Red',
+  },
+  {
+    question: 'Which color is a strawberry?',
+    choices: ['Yellow', 'Blue', 'Red', 'Orange'],
+    correctAnswer: 'Red',
+  },
+  {
+    question: 'What color is the moon at night?',
+    choices: ['Brown', 'Yellow', 'White', 'Black'],
+    correctAnswer: 'White',
+  },
+  {
+    question: 'Which color is a firetruck?',
+    choices: ['Green', 'Blue', 'Yellow', 'Red'],
+    correctAnswer: 'Red',
+  },
+  {
+    question: 'What color is a frog?',
+    choices: ['Blue', 'Green', 'Purple', 'Pink'],
+    correctAnswer: 'Green',
+  },
+  {
+    question: 'Which color is a school bus?',
+    choices: ['Brown', 'Yellow', 'Red', 'Green'],
+    correctAnswer: 'Yellow',
+  },
+  {
+    question: 'What color are your shoes?',
+    choices: ['Yellow', 'Blue', 'Green', 'Brown'],
+    correctAnswer: 'Varies',
+  },
+  {
+    question: 'Which color is a cat?',
+    choices: ['Purple', 'Orange', 'Brown', 'Black'],
+    correctAnswer: 'Varies',
+  },
+  {
+    question: 'What color is the ocean?',
+    choices: ['Green', 'Blue', 'Yellow', 'Pink'],
+    correctAnswer: 'Blue',
+  },
+  {
+    question: 'Which color is a duck?',
+    choices: ['Pink', 'Yellow', 'Red', 'Blue'],
+    correctAnswer: 'Varies',
+  },
+  {
+    question: 'What color is a cow?',
+    choices: ['Black', 'Yellow', 'Brown', 'Purple'],
+    correctAnswer: 'Varies',
+  },
+  {
+    question: 'Which color is a car?',
+    choices: ['Green', 'Red', 'Yellow', 'Blue'],
+    correctAnswer: 'Varies',
+  },
+  {
+    question: 'What color are your pajamas?',
+    choices: ['Pink', 'Blue', 'Yellow', 'Green'],
+    correctAnswer: 'Varies',
+  },
+  {
+    question: 'Which color is a flower?',
+    choices: ['Brown', 'Yellow', 'Red', 'Blue'],
+    correctAnswer: 'Varies',
+  },
+  {
+    question: 'What color is a butterfly?',
+    choices: ['Green', 'Yellow', 'Blue', 'Red'],
+    correctAnswer: 'Varies',
+  },
+  {
+    question: 'Which color is a ball?',
+    choices: ['Pink', 'Yellow', 'Red', 'Blue'],
+    correctAnswer: 'Varies',
+  },
+];
